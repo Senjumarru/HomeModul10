8a0265f2e60f83d377ee6232add4bc829802d9c8
Senjumarru <alinursarsekov315@gmail.com>
2024-11-12 20:09:29 +0500

    Добавьте файлы проекта.

---
+++ HomeComposite/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class FileSystemComponent
+{
+    public string Name { get; set; }
+
+    public FileSystemComponent(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void Display(int indent = 0);
+    public abstract int GetSize();
+}
+
+class File : FileSystemComponent
+{
+    public int Size { get; private set; }
+
+    public File(string name, int size) : base(name)
+    {
+        Size = size;
+    }
+
+    public override void Display(int indent = 0)
+    {
+        Console.WriteLine(new String(' ', indent) + $"Файл: {Name} (Размер: {Size})");
+    }
+
+    public override int GetSize() => Size;
+}
+
+class Directory : FileSystemComponent
+{
+    private List<FileSystemComponent> components = new List<FileSystemComponent>();
+
+    public Directory(string name) : base(name) { }
+
+    public void Add(FileSystemComponent component)
+    {
+        if (!components.Contains(component))
+        {
+            components.Add(component);
+        }
+    }
+
+    public void Remove(FileSystemComponent component)
+    {
+        components.Remove(component);
+    }
+
+    public override void Display(int indent = 0)
+    {
+        Console.WriteLine(new String(' ', indent) + $"Папка: {Name}");
+        foreach (var component in components)
+        {
+            component.Display(indent + 2);
+        }
+    }
+
+    public override int GetSize()
+    {
+        int totalSize = 0;
+        foreach (var component in components)
+        {
+            totalSize += component.GetSize();
+        }
+        return totalSize;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var rootDir = new Directory("Root");
+
+        var documents = new Directory("Documents");
+        documents.Add(new File("Resume.pdf", 120));
+        documents.Add(new File("ProjectPlan.docx", 350));
+
+        var pictures = new Directory("Pictures");
+        pictures.Add(new File("Vacation.jpg", 2000));
+        pictures.Add(new File("Profile.png", 500));
+
+        rootDir.Add(documents);
+        rootDir.Add(pictures);
+
+        var downloads = new Directory("Downloads");
+        downloads.Add(new File("Setup.exe", 15000));
+        downloads.Add(new File("Guide.pdf", 700));
+
+        rootDir.Add(downloads);
+
+        Console.WriteLine("Содержимое файловой системы:");
+        rootDir.Display();
+
+        Console.WriteLine($"\nОбщий размер файловой системы: {rootDir.GetSize()} KB");
+    }
+}
---
+++ HomeModul10/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+class TV
+{
+    public void On() => Console.WriteLine("Телевизор включен");
+    public void Off() => Console.WriteLine("Телевизор выключен");
+    public void SetChannel(int channel) => Console.WriteLine($"Канал установлен на {channel}");
+}
+
+class AudioSystem
+{
+    public void On() => Console.WriteLine("Аудиосистема включена");
+    public void Off() => Console.WriteLine("Аудиосистема выключена");
+    public void SetVolume(int volume) => Console.WriteLine($"Громкость установлена на {volume}");
+}
+
+class DVDPlayer
+{
+    public void Play() => Console.WriteLine("Воспроизведение DVD началось");
+    public void Pause() => Console.WriteLine("Воспроизведение DVD приостановлено");
+    public void Stop() => Console.WriteLine("Воспроизведение DVD остановлено");
+}
+
+class GameConsole
+{
+    public void On() => Console.WriteLine("Игровая консоль включена");
+    public void StartGame() => Console.WriteLine("Игра запущена");
+}
+
+class HomeTheaterFacade
+{
+    private TV tv;
+    private AudioSystem audioSystem;
+    private DVDPlayer dvdPlayer;
+    private GameConsole gameConsole;
+
+    public HomeTheaterFacade(TV tv, AudioSystem audioSystem, DVDPlayer dvdPlayer, GameConsole gameConsole)
+    {
+        this.tv = tv;
+        this.audioSystem = audioSystem;
+        this.dvdPlayer = dvdPlayer;
+        this.gameConsole = gameConsole;
+    }
+
+    public void WatchMovie()
+    {
+        tv.On();
+        audioSystem.On();
+        dvdPlayer.Play();
+        Console.WriteLine("Наслаждайтесь фильмом!");
+    }
+
+    public void StopMovie()
+    {
+        dvdPlayer.Stop();
+        audioSystem.Off();
+        tv.Off();
+        Console.WriteLine("Фильм остановлен.");
+    }
+
+    public void StartGame()
+    {
+        gameConsole.On();
+        gameConsole.StartGame();
+        Console.WriteLine("Начинаем играть!");
+    }
+
+    public void StopGame()
+    {
+        gameConsole.On();
+        Console.WriteLine("Игра остановлена.");
+    }
+
+    public void ListenToMusic()
+    {
+        tv.On();
+        audioSystem.On();
+        audioSystem.SetVolume(30);
+        Console.WriteLine("Прослушивание музыки.");
+    }
+
+    public void SetVolume(int volume)
+    {
+        audioSystem.SetVolume(volume);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var tv = new TV();
+        var audioSystem = new AudioSystem();
+        var dvdPlayer = new DVDPlayer();
+        var gameConsole = new GameConsole();
+
+        var homeTheater = new HomeTheaterFacade(tv, audioSystem, dvdPlayer, gameConsole);
+
+        homeTheater.WatchMovie();
+        homeTheater.SetVolume(20);
+        homeTheater.StopMovie();
+
+        homeTheater.StartGame();
+        homeTheater.StopGame();
+
+        homeTheater.ListenToMusic();
+    }
+}
